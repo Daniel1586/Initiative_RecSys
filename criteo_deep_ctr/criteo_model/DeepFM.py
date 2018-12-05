839bc6c92336b35fa7515467a5d04c17e1990643
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-12-05 21:29:45 +0800

    update criteo DeepFM model

--- criteo_deep_ctr/criteo_model/DeepFM.py
+++ criteo_deep_ctr/criteo_model/DeepFM.py
@@ -55,29 +55,24 @@ tf.app.flags.DEFINE_string("flag_dir", '', "flag name for different test time")
 tf.app.flags.DEFINE_string("servable_model_dir", '', "export servable model for TensorFlow Serving")
 tf.app.flags.DEFINE_boolean("clear_existing_model", False, "clear existing model or not")
 
-#1 1:0.5 2:0.03519 3:1 4:0.02567 7:0.03708 8:0.01705 9:0.06296 10:0.18185 11:0.02497 12:1 14:0.02565 15:0.03267 17:0.0247 18:0.03158 20:1 22:1 23:0.13169 24:0.02933 27:0.18159 31:0.0177 34:0.02888 38:1 51:1 63:1 132:1 164:1 236:1
+
+# 0 1:0.05 2:0.004983 3:0.05 4:0 5:0.021594 6:0.008 7:0.15 8:0.04 9:0.362 10:0.166667 11:0.2 12:0 13:0.04
 def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
-    print('Parsing', filenames)
-    def decode_libsvm(line):
-        #columns = tf.decode_csv(value, record_defaults=CSV_COLUMN_DEFAULTS)
-        #features = dict(zip(CSV_COLUMNS, columns))
-        #labels = features.pop(LABEL_COLUMN)
+    print('Parsing ---- ', filenames)
+
+    def decode_dataset(line):
         columns = tf.string_split([line], ' ')
         labels = tf.string_to_number(columns.values[0], out_type=tf.float32)
         splits = tf.string_split(columns.values[1:], ':')
-        id_vals = tf.reshape(splits.values,splits.dense_shape)
-        feat_ids, feat_vals = tf.split(id_vals,num_or_size_splits=2,axis=1)
+        id_vals = tf.reshape(splits.values, splits.dense_shape)
+        feat_ids, feat_vals = tf.split(id_vals, num_or_size_splits=2, axis=1)
         feat_ids = tf.string_to_number(feat_ids, out_type=tf.int32)
         feat_vals = tf.string_to_number(feat_vals, out_type=tf.float32)
-        #feat_ids = tf.reshape(feat_ids,shape=[-1,FLAGS.field_size])
-        #for i in range(splits.dense_shape.eval()[0]):
-        #    feat_ids.append(tf.string_to_number(splits.values[2*i], out_type=tf.int32))
-        #    feat_vals.append(tf.string_to_number(splits.values[2*i+1]))
-        #return tf.reshape(feat_ids,shape=[-1,field_size]), tf.reshape(feat_vals,shape=[-1,field_size]), labels
         return {"feat_ids": feat_ids, "feat_vals": feat_vals}, labels
 
     # Extract lines from input files using the Dataset API, can pass one filename or filename list
-    dataset = tf.data.TextLineDataset(filenames).map(decode_libsvm, num_parallel_calls=10).prefetch(500000)    # multi-thread pre-process then prefetch
+    # multi-thread pre-process then prefetch
+    dataset = tf.data.TextLineDataset(filenames).map(decode_dataset, num_parallel_calls=10).prefetch(2000)
 
     # Randomizes input using a window of 256 elements (read into memory)
     if perform_shuffle:
@@ -85,28 +80,25 @@ def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
 
     # epochs from blending together.
     dataset = dataset.repeat(num_epochs)
-    dataset = dataset.batch(batch_size) # Batch size to use
+    dataset = dataset.batch(batch_size)
 
-    #return dataset.make_one_shot_iterator()
     iterator = dataset.make_one_shot_iterator()
     batch_features, batch_labels = iterator.get_next()
-    #return tf.reshape(batch_ids,shape=[-1,field_size]), tf.reshape(batch_vals,shape=[-1,field_size]), batch_labels
     return batch_features, batch_labels
 
+
 def model_fn(features, labels, mode, params):
-    """Bulid Model function f(x) for Estimator."""
-    #------hyperparameters----
+
+    # ----- hyper-parameters ----- #
     field_size = params["field_size"]
     feature_size = params["feature_size"]
     embedding_size = params["embedding_size"]
     l2_reg = params["l2_reg"]
     learning_rate = params["learning_rate"]
-    #batch_norm_decay = params["batch_norm_decay"]
-    #optimizer = params["optimizer"]
-    layers  = map(int, params["deep_layers"].split(','))
+    layers = map(int, params["deep_layers"].split(','))
     dropout = map(float, params["dropout"].split(','))
 
-    #------bulid weights------
+    # ----- initial weights ----- #
     FM_B = tf.get_variable(name='fm_bias', shape=[1], initializer=tf.constant_initializer(0.0))
     FM_W = tf.get_variable(name='fm_w', shape=[feature_size], initializer=tf.glorot_normal_initializer())
     FM_V = tf.get_variable(name='fm_v', shape=[feature_size, embedding_size], initializer=tf.glorot_normal_initializer())
@@ -224,6 +216,7 @@ def model_fn(features, labels, mode, params):
     #        predictions={"prob": pred},
     #        eval_metric_ops=eval_metric_ops)
 
+
 def batch_norm_layer(x, train_phase, scope_bn):
     bn_train = tf.contrib.layers.batch_norm(x, decay=FLAGS.batch_norm_decay, center=True, scale=True, updates_collections=None, is_training=True,  reuse=None, scope=scope_bn)
     bn_infer = tf.contrib.layers.batch_norm(x, decay=FLAGS.batch_norm_decay, center=True, scale=True, updates_collections=None, is_training=False, reuse=True, scope=scope_bn)
@@ -328,7 +321,6 @@ def main(_):
     env_set()
 
     print('========== 4.Build tasks and algorithm model...')
-    print('to be modified')
     model_params = {
         "field_size": FLAGS.field_size,
         "feature_size": FLAGS.feature_size,
@@ -339,34 +331,35 @@ def main(_):
         "deep_layers": FLAGS.deep_layers,
         "dropout": FLAGS.dropout
     }
-    config = tf.estimator.RunConfig().replace(session_config = tf.ConfigProto(device_count={'GPU':0, 'CPU':FLAGS.num_threads}),
-            log_step_count_steps=FLAGS.log_steps, save_summary_steps=FLAGS.log_steps)
-    DeepFM = tf.estimator.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir, params=model_params, config=config)
+    config = tf.estimator.RunConfig().replace(
+        session_config=tf.ConfigProto(device_count={'GPU': 0, 'CPU': FLAGS.num_threads}),
+        log_step_count_steps=FLAGS.log_steps, save_summary_steps=FLAGS.log_steps)
+    deepfm = tf.estimator.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir,
+                                    params=model_params, config=config)
 
-    if FLAGS.task_type == 'train':
-        train_spec = tf.estimator.TrainSpec(input_fn=lambda: input_fn(tr_files, num_epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size))
-        eval_spec = tf.estimator.EvalSpec(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size), steps=None, start_delay_secs=1000, throttle_secs=1200)
-        tf.estimator.train_and_evaluate(DeepFM, train_spec, eval_spec)
-    elif FLAGS.task_type == 'eval':
-        DeepFM.evaluate(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size))
-    elif FLAGS.task_type == 'infer':
-        preds = DeepFM.predict(input_fn=lambda: input_fn(te_files, num_epochs=1, batch_size=FLAGS.batch_size), predict_keys="prob")
+    if FLAGS.task_mode == 'train':
+        train_spec = tf.estimator.TrainSpec(
+            input_fn=lambda: input_fn(train_files, batch_size=FLAGS.batch_size, num_epochs=FLAGS.num_epochs))
+        eval_spec = tf.estimator.EvalSpec(
+            input_fn=lambda: input_fn(valid_files, batch_size=FLAGS.batch_size, num_epochs=1),
+            steps=None, start_delay_secs=1000, throttle_secs=1200)
+        tf.estimator.train_and_evaluate(deepfm, train_spec, eval_spec)
+    elif FLAGS.task_mode == 'eval':
+        deepfm.evaluate(input_fn=lambda: input_fn(valid_files, num_epochs=1, batch_size=FLAGS.batch_size))
+    elif FLAGS.task_mode == 'infer':
+        preds = deepfm.predict(
+            input_fn=lambda: input_fn(tests_files, num_epochs=1, batch_size=FLAGS.batch_size),
+            predict_keys="prob")
         with open(FLAGS.data_dir+"/pred.txt", "w") as fo:
             for prob in preds:
                 fo.write("%f\n" % (prob['prob']))
-    elif FLAGS.task_type == 'export':
-        #feature_spec = tf.feature_column.make_parse_example_spec(feature_columns)
-        #feature_spec = {
-        #    'feat_ids': tf.FixedLenFeature(dtype=tf.int64, shape=[None, FLAGS.field_size]),
-        #    'feat_vals': tf.FixedLenFeature(dtype=tf.float32, shape=[None, FLAGS.field_size])
-        #}
-        #serving_input_receiver_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)
+    elif FLAGS.task_mode == 'export':
         feature_spec = {
             'feat_ids': tf.placeholder(dtype=tf.int64, shape=[None, FLAGS.field_size], name='feat_ids'),
             'feat_vals': tf.placeholder(dtype=tf.float32, shape=[None, FLAGS.field_size], name='feat_vals')
         }
         serving_input_receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
-        DeepFM.export_savedmodel(FLAGS.servable_model_dir, serving_input_receiver_fn)
+        deepfm.export_savedmodel(FLAGS.servable_model_dir, serving_input_receiver_fn)
 
 
 if __name__ == "__main__":
