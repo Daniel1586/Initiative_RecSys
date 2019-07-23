a145d6e39c9d9bc033eb587bace15bb079e56681
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2019-07-23 20:44:02 +0800

    update data_criteo_feature.py

--- data/data_criteo_feature.py
+++ data/data_criteo_feature.py
@@ -11,7 +11,7 @@ This code is referenced from PaddlePaddle models.
 (https://github.com/PaddlePaddle/models/blob/develop/legacy/deep_fm/preprocess.py)
 --For numeric features, clipped and normalized.
 --For categorical features, removed long-tailed data appearing less than 200 times.
-########## TF Version: 1.8.0 ##########
+########## TF Version: 1.8.0/Python Version: 3.6 ##########
 """
 
 import os
@@ -28,6 +28,7 @@ categorical_features = range(14, 40)
 # Clip numeric features. The clip point for each numeric feature is
 # derived from the 95% quantile of the total values in each feature
 # 数值特征的阈值,若数值特征超过阈值,则该特征值置为阈值[剔除异常值]
+# 阈值来源--https://github.com/PaddlePaddle/models/blob/develop/legacy/deep_fm/preprocess.py
 numeric_clip = [20, 600, 100, 50, 64000, 500, 100, 50, 500, 10, 10, 10, 50]
 
 
@@ -110,15 +111,15 @@ def preprocess(datain_dir, dataou_dir):
     vectors are combined into one sparse binary vector.
     """
 
-    print('========== 1.Preprocess numeric and categorical features...')
+    print("========== 1.Preprocess numeric and categorical features...")
     n_feat = NumericFeatureGenerator(len(numeric_features))
-    n_feat.build(datain_dir + 'train.txt', numeric_features)
+    n_feat.build(datain_dir + "train.txt", numeric_features)
     c_feat = CategoryDictGenerator(len(categorical_features))
-    c_feat.build(datain_dir + 'train.txt', categorical_features, cutoff=FLAGS.cut_off)
+    c_feat.build(datain_dir + "train.txt", categorical_features, cutoff=FLAGS.cut_off)
 
-    print('========== 2.Generate index of feature embedding ...')
+    print("========== 2.Generate index of feature embedding ...")
     # 生成数值特征编号: I1-I13
-    output = open(dataou_dir + 'embed.set', 'w')
+    output = open(dataou_dir + "embed.set", 'w')
     for i in numeric_features:
         output.write("{0} {1}\n".format('I'+str(i), i))
 
@@ -132,11 +133,11 @@ def preprocess(datain_dir, dataou_dir):
             output.write("{0} {1}\n".format('C'+str(i)+'|'+key, c_feat_offset[i - 1]+val+1))
 
     random.seed(0)
-    # 90% data are used for training, and 10% data are used for validation.
-    print('========== 3.Generate train/valid/test dataset ...')
-    with open(dataou_dir + 'train.set', 'w') as out_train:
-        with open(dataou_dir + 'valid.set', 'w') as out_valid:
-            with open(datain_dir + 'train.txt', 'r') as f:
+    # 90% data are used for training, and 10% data are used for validation
+    print("========== 3.Generate train/valid/test dataset ...")
+    with open(dataou_dir + "train.set", 'w') as out_train:
+        with open(dataou_dir + "valid.set", 'w') as out_valid:
+            with open(datain_dir + "train.txt", 'r') as f:
                 for line in f:
                     features = line.rstrip('\n').split('\t')
 
@@ -157,8 +158,8 @@ def preprocess(datain_dir, dataou_dir):
                     else:
                         out_valid.write("{0} {1}\n".format(label, ' '.join(feat_val)))
 
-    with open(dataou_dir + 'tests.set', 'w') as out_test:
-        with open(datain_dir + 'test.txt', 'r') as f:
+    with open(dataou_dir + "tests.set", 'w') as out_test:
+        with open(datain_dir + "test.txt", 'r') as f:
             for line in f:
                 features = line.rstrip('\n').split('\t')
 
@@ -178,12 +179,11 @@ def preprocess(datain_dir, dataou_dir):
 if __name__ == "__main__":
     run_mode = 0        # 0: windows环境
     if run_mode == 0:
-        dir_data = os.path.dirname(os.getcwd())
-        dir_datain = dir_data + '\\criteo_data_raw\\'
-        dir_dataou = dir_data + '\\criteo_data_set\\'
+        dir_datain = os.getcwd() + "\\data_raw_criteo\\"
+        dir_dataou = os.getcwd() + "\\data_set_criteo\\"
     else:
-        dir_datain = ''
-        dir_dataou = ''
+        dir_datain = ""
+        dir_dataou = ""
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--threads", type=int, default=2, help="threads num")
@@ -191,10 +191,10 @@ if __name__ == "__main__":
     parser.add_argument("--data_ou", type=str, default=dir_dataou, help="data_out dir")
     parser.add_argument("--cut_off", type=int, default=200, help="cutoff long-tailed categorical values")
     FLAGS, unparsed = parser.parse_known_args()
-    print('threads -------------- ', FLAGS.threads)
-    print('input_dir ------------ ', FLAGS.data_in)
-    print('output_dir ----------- ', FLAGS.data_ou)
-    print('cutoff --------------- ', FLAGS.cut_off)
+    print("threads -------------- ", FLAGS.threads)
+    print("input_dir ------------ ", FLAGS.data_in)
+    print("output_dir ----------- ", FLAGS.data_ou)
+    print("cutoff --------------- ", FLAGS.cut_off)
 
     # 特征预处理
     preprocess(FLAGS.data_in, FLAGS.data_ou)
