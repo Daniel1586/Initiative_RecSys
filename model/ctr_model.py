f6a0b4458d6ddf85dc01f59e14c096d8b3b2e5a5
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2019-07-24 22:36:27 +0800

    update ctr_model.py

--- model/ctr_model.py
+++ model/ctr_model.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
-import json
-import glob
-import random
-import shutil
 import tensorflow as tf
-from datetime import date, timedelta
 
 
 # LR: Predicting Clicks - Estimating the Click-Through Rate for New Ads.
 def model_lr(features, labels, mode, params):
 
-    # ---------- hyper-parameters ---------- #
+    # --------------- hyper-parameters --------------- #
     feature_size = params["feature_size"]
     field_size = params["field_size"]
+    loss_mode = params["loss_mode"]
+    optimizer = params["optimizer"]
     learning_rate = params["learning_rate"]
     l2_reg_lambda = params["l2_reg_lambda"]
 
-    # ---------- initial weights ----------- #
+    # --------------- initial weights ---------------- #
     # [numeric_feature, one-hot categorical_feature]
     coe_b = tf.get_variable(name="coe_b", shape=[1], initializer=tf.constant_initializer(0.0))
     coe_w = tf.get_variable(name="coe_w", shape=[feature_size], initializer=tf.glorot_normal_initializer())
 
-    # ---------- reshape feature ----------- #
+    # --------------- reshape feature ---------------- #
     feat_idx = features["feat_idx"]         # 非零特征位置[batch_size, field_size, 1]
     feat_idx = tf.reshape(feat_idx, shape=[-1, field_size])     # [Batch, Field]
     feat_val = features["feat_val"]         # 非零特征的值[batch_size, field_size, 1]
     feat_val = tf.reshape(feat_val, shape=[-1, field_size])     # [Batch, Field]
 
-    # ------------- define f(x) ------------ #
+    # ------------------ define f(x) ----------------- #
     # LR: y = b + sum<wi,xi>
     with tf.variable_scope("First-Order"):
         feat_wgt = tf.nn.embedding_lookup(coe_w, feat_idx)              # [Batch, Field]
@@ -41,7 +37,7 @@ def model_lr(features, labels, mode, params):
         y_hat = y_b + y_w                                               # [Batch]
         y_pred = tf.nn.sigmoid(y_hat)                                   # [Batch]
 
-    # ----- mode: predict/evaluate/train ----- #
+    # ---------- mode: predict/evaluate/train ---------- #
     # predict: 不计算loss/metric; evaluate: 不进行梯度下降和参数更新
 
     # Provide an estimator spec for 'ModeKeys.PREDICT'
@@ -53,7 +49,7 @@ def model_lr(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, export_outputs=export_outputs)
 
     # Provide an estimator spec for 'ModeKeys.EVAL'
-    if FLAGS.loss_mode == "log_loss":
+    if loss_mode == "log_loss":
         loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=y_hat)) +\
                l2_reg_lambda * tf.nn.l2_loss(coe_w)
     else:
@@ -64,17 +60,17 @@ def model_lr(features, labels, mode, params):
                                           eval_metric_ops=eval_metric_ops)
 
     # Provide an estimator spec for 'ModeKeys.TRAIN'
-    if FLAGS.optimizer == "Adam":
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8)
-    elif FLAGS.optimizer == "Adagrad":
-        optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate, initial_accumulator_value=1e-8)
-    elif FLAGS.optimizer == "Momentum":
-        optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.95)
-    elif FLAGS.optimizer == "Ftrl":
-        optimizer = tf.train.FtrlOptimizer(learning_rate)
+    if optimizer == "Adam":
+        opt_mode = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8)
+    elif optimizer == "Adagrad":
+        opt_mode = tf.train.AdagradOptimizer(learning_rate=learning_rate, initial_accumulator_value=1e-8)
+    elif optimizer == "Momentum":
+        opt_mode = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.95)
+    elif optimizer == "Ftrl":
+        opt_mode = tf.train.FtrlOptimizer(learning_rate)
     else:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+        opt_mode = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    train_op = opt_mode.minimize(loss, global_step=tf.train.get_global_step())
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, loss=loss, train_op=train_op)
